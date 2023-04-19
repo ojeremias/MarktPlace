c4b1be8709467bece95de251110cc43518de6ab7
Leonardo José <118532015+l30n44rd0std@users.noreply.github.com>
2023-04-19 11:38:47 -0300

    preparando meu component para o layout

--- projeto/src/components/ComponentBrinquedo/BrinquedoComponent.js
+++ projeto/src/components/ComponentBrinquedo/BrinquedoComponent.js
@@ -3,12 +3,17 @@ import React from "react";
 function BrinquedoComponent ({ brinquedoProdutos, adicionarFavoritos, adicionarDenuciado}) {
 
     return (
-        <div className="box-produto">
-            <h1>Brinquedos - Leonardo</h1>
+        <div className="container">
+            <h1>Brinquedos - Leonardo </h1> 
             {
                 brinquedoProdutos.map(p =>
-                    <div key={p.id}>
-                        {p.nome} R${p.preco}
+                    <div key={p.id} className="card">
+                        <div className="container">
+                        <h4><b>{p.nome}</b></h4>
+                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1Av.7PVXXXXb5XFXXq6xXFXXXv%2F100-pieces-Wooden-Puzzle-Toy-Cartoon-3D-Wood-Jigsaw-Puzzle-Iron-Box-Package-for-Child-Educational.jpg&f=1&nofb=1&ipt=c4c4fcbdd5285866fcc0f16c232340e8bafaaf58b771d415b4f5663588828337&ipo=images"/>
+                        <p>R${p.preco}</p>
+
+                        </div>
                         <button onClick={()=>{
                             adicionarFavoritos(p);
                             console.log(p)
