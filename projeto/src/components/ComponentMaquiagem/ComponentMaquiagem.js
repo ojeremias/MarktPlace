47b8fdc71151527be98983efb9ce0b5086e50244
VitoriaGoncalvez <107042599+VitoriaGoncalvez@users.noreply.github.com>
2023-04-20 11:05:21 -0300

    atualizado

--- projeto/src/components/ComponentMaquiagem/ComponentMaquiagem.js
+++ projeto/src/components/ComponentMaquiagem/ComponentMaquiagem.js
@@ -2,7 +2,7 @@ import React from 'react'
 export default function MaquiagemCompenent(props) {
     console.log(props)
   return (
-    <div className="">
+    <div class="">
         {props.maquiagemProdutos[0].nome}, {props.maquiagemProdutos[1].nome}, {props.maquiagemProdutos[2].nome}
     </div>
   )
