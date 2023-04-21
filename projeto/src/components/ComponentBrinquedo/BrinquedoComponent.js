7668a27916dbaff207414bbd2bcca2e678e2b024
l30n44rd0 <118532015+l30n44rd0std@users.noreply.github.com>
2023-04-21 10:29:40 -0300

    ++

--- projeto/src/components/ComponentBrinquedo/BrinquedoComponent.js
+++ projeto/src/components/ComponentBrinquedo/BrinquedoComponent.js
@@ -2,19 +2,23 @@ import React from "react";
 
 function BrinquedoComponent ({ brinquedoProdutos, adicionarFavoritos, adicionarDenuciado}) {
 
+const img = ['', './img/quebracabeca.jpg', './img/domino.jpg', './img/uno.jpg',];
     return (
-        <div className="container-leonardo">
-            <h1>Brinquedos - Leonardo </h1> 
+        <div className="container-brinquedo">
+            <span className="header-brinquedo"><h1>Brinquedos - Leonardo </h1></span> 
             {
                 brinquedoProdutos.map(p =>
-                    <div key={p.id} className="card-leonardo">
-                        <div className="container-leonardo">
-                        <h4><b>{p.nome}</b></h4>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1Av.7PVXXXXb5XFXXq6xXFXXXv%2F100-pieces-Wooden-Puzzle-Toy-Cartoon-3D-Wood-Jigsaw-Puzzle-Iron-Box-Package-for-Child-Educational.jpg&f=1&nofb=1&ipt=c4c4fcbdd5285866fcc0f16c232340e8bafaaf58b771d415b4f5663588828337&ipo=images"/>
-                        <p>R${p.preco}</p>
+                    <div key={p.id} className="cards-brinquedo">
+                        <div className="card-brinquedo">
 
+                        <div className="imgBrinquedo">
+                            <img src={img[p.id]}></img>                        
                         </div>
-                        <div>
+
+                        <div className="namePrice">
+                            <h3>{p.nome}</h3>
+                            <p>R${p.preco}</p>
+                        </div>
 
                         <button onClick={()=>{
                             adicionarFavoritos(p);
