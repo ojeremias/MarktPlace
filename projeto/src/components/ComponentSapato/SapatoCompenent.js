a90cfaf28d996f6d41f67d002a2f6ac26b540aed
roberto_farias <75376776+roberto06rhz@users.noreply.github.com>
2023-04-27 08:47:45 -0300

    skks

--- projeto/src/components/ComponentSapato/SapatoCompenent.js
+++ projeto/src/components/ComponentSapato/SapatoCompenent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+<<<<<<< Updated upstream
 import "./sapatos.css"
 function SapatosCompenent({sapatoProdutos,categoria,onAdd, adicionarFavoritos,adicionarDenuciado}) {
   
@@ -57,5 +58,58 @@ function SapatosCompenent({sapatoProdutos,categoria,onAdd, adicionarFavoritos,ad
             </div>
    </div>
  )
+=======
+import "./sapatos.css" 
+ function SapatosCompenent({sapatoProdutos,categoria,onAdd, adicionarFavoritos,adicionarDenuciado}) {
+    
+  return (
+    <div>
+      <h1>SAPATOS ROBERTO/CLICIA</h1>
+    <div className="sapConteiner">
+        {
+
+          sapatoProdutos.map(p => 
+            <div key={p.id}>
+              <div className="SapInfor">
+              
+              <div>
+
+                <div>
+                  <img src='54'></img>
+                </div>
+
+              <div className='sapTexto'>
+              Nome:{p.nome}
+              </div>
+
+              <div className='sapTexto'>
+              R$:{p.preco}
+              </div>
+
+              </div>
+
+              <div>
+              <button id='sapBotao' onClick={()=>{
+               adicionarFavoritos(p);
+                console.log(p)
+              }}>Adicionar</button>
+
+              <button id='sapBotao' onClick={()=>{
+                adicionarDenuciado(p);
+              }}>Denuciar</button>
+              </div>
+
+
+              </div>
+
+              </div>
+
+              )
+
+        }
+    </div>
+    </div>
+  )
+>>>>>>> Stashed changes
 }
 export default SapatosCompenent
