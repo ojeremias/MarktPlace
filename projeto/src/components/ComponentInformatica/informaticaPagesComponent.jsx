3c74dacfae25c4947aa824c22108fc0c8d69cb9b
Antonio <avps2@aluno.ifal.edu.br>
2023-05-11 11:17:31 -0300

    git

---
+++ projeto/src/components/ComponentInformatica/informaticaPagesComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function informaticaPagesComponent() {
+  return (
+    <div>
+        
+        <h1>informaticaPagesComponent</h1>
+        </div>
+  )
+}
