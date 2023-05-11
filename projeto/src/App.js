17f921d45cde45f4309206f26b3da6814f7430d8
Leonardo José <118532015+l30n44rd0std@users.noreply.github.com>
2023-05-11 11:26:57 -0300

    .

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MarketPlace from './components/MarketPlace/MarketPlace';
+import BrinquedosPageComponent from './components/ComponentBrinquedo/BrinquedosPageComponent.jsx';
 
 function App() {
 
     return (
-      <MarketPlace/>
+      <Routes>
+        <Route path="/" element={<MarketPlace/>}></Route>
+        <Route path="/brinquedos" element={<BrinquedosPageComponent/>}></Route>
+      </Routes>
+      
   );
 }
 export default App;
