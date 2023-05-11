84a5067a7d61dfbc9923e7f0d817439cb24b16f5
jeallyson <118531980+jeallyson@users.noreply.github.com>
2023-05-11 11:22:09 -0300

    component

---
+++ projeto/src/components/ComponentTecnologia/PageComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link, Routes } from 'react-router-dom'
+import MarketPlace from '../MarketPlace/MarketPlace'
+
+function PageComponent() {
+//   return (
+
+//     <Routes>
+//         <Route path="/" element={<MarketPlace/>}></Route>
+//     </Routes>
+   
+//   )
+}
+
+export default PageComponent
