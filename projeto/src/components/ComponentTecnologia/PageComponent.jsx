8e4dfd4468cf6a543890c1b8abb4081bab836505
jeallyson <118531980+jeallyson@users.noreply.github.com>
2023-05-12 08:53:50 -0300

    n tô entendo mais nada

--- projeto/src/components/ComponentTecnologia/PageComponent.jsx
+++ projeto/src/components/ComponentTecnologia/PageComponent.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Link, Routes } from 'react-router-dom'
-import MarketPlace from '../MarketPlace/MarketPlace'
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import MarketPlace from '../MarketPlace';
+import TecnologiaComponent from './TecnologiaComponent';
 
 function PageComponent() {
-//   return (
-
-//     <Routes>
-//         <Route path="/" element={<MarketPlace/>}></Route>
-//     </Routes>
-   
-//   )
+  return (
+    <Routes>
+      <Route path="/" element={<MarketPlace />} />
+      <Route path="tecnologia" element={<TecnologiaComponent />} />
+      <Link to="tecnologia">Clique</Link>
+    </Routes>
+  );
 }
 
-export default PageComponent
+export default PageComponent;
