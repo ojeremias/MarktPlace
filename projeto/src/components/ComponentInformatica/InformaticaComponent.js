57f17e8ad3b89c4deceaac5aa826573246044960
Antonio <avps2@aluno.ifal.edu.br>
2023-05-17 10:52:47 -0300

    usee antnkio

--- projeto/src/components/ComponentInformatica/InformaticaComponent.js
+++ projeto/src/components/ComponentInformatica/InformaticaComponent.js
@@ -1,58 +1,102 @@
-
-import  { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import '../../App.css';
 import './InformaticaComponent.css';
 
 
+function InformaticaComponent({ adicionarFavoritos, adicionarDenuciado }) {
 
-export default function InformaticaComponent({produtos, adicionarFavoritos, adicionarDenuciado}){
-  
-  const [quantidade, setQuantidade] = useState(0)
-  const [quantidade1, setQuantidade1] = useState(0)
+  const [infoContar, setinfoContar] = useState(0);
 
+  const [listaInfor, setlistaInfor] = useState([]);
+  const [ordem, setOrdem] = useState('preco')
 
-  return(
+  function quantFavSaoContar() {
+    setinfoContar(infoContar + 1)
+  }
+  async function getProdutos(categoria) {
+    const response = await fetch(`https://dummyjson.com/products/category/${categoria}`);
+    const dadosApi = await response.json();
+    const result = dadosApi.products.map(
+      product => ({
+        id: product.id,
+        nome: product.title,
+        preco: product.price,
+        img: product.thumbnail,
+        marca: product.brand,
+        estoque: product.stock,
+        avaliacao: product.rating
+      })
+    );
 
-        <div className='containerInformatica' >
-        <h1>Setor Informatica (Antonio)</h1>
-          <div className='divQuantidades'>
-          
-            Produtos que foram Adicionados : {quantidade } {' '}
-            Produtos que foram denunciados : {quantidade1}
-     
-          </div>
-    <div className='telaInformatica'>
-      {
-          produtos.map(p => 
-            <div key={p.id} className="telaInformaticaCard">
-          
-          <div>
-          Produto: {p.nome}
-          </div>
-          <div className='telaInformaticaImg'>
-          <img className='img' src={p.img} alt='Img Antonio' ></img>
-          </div>
-          <div>
-            Preço: R${p.preco}
-          </div> 
-          <div className="button" onClick={()=>{
-                            adicionarFavoritos(p);
-                           
-                            setQuantidade(quantidade+1);
-                           
+    return result;
+  }
 
-                        }}>Adicionar</div>
-
-                        <div className="button" onClick={()=>{
-                            adicionarDenuciado(p);
-                            setQuantidade1(quantidade1+1);
-                        }}>Denuciar</div>
-          </div>
-        )
-    }
-    </div>
-    </div>
+  useEffect(() => {
    
-        
-    )
+    const load = async () => {
+      const informaticaNotebook = await getProdutos('laptops');
+      const informaticaSmartphone = await getProdutos('smartphones');
+
+      setlistaInfor([...informaticaSmartphone, ...informaticaNotebook]);
+    }
+    load();
+  }, []);
+
+  useEffect(() => {
+    const listaOrdenada = listaInfor.sort(function(a,b) {
+      return a[ordem] < b[ordem] ? -1 : a[ordem] > b[ordem] ? 1 : 0;
+    });
+
+    setlistaInfor(listaOrdenada);
+  }, [ordem]);
+
+
+  return (
+    <div className='containerInformatica'>
+      <h1>Informática do Tonho</h1>
+      <span> Informatica adicionado <b>favoritados</b>:{infoContar}</span>
+      <select onChange={(op) => setOrdem(op.target.value)}>
+          <option value='preco'>Preço</option>
+          <option value='estoque'>Estoque</option>
+          <option value='avaliacao'>Avaliação</option>
+        </select>
+      <div className="telaInformatica">
+        {
+
+          listaInfor.map(p =>
+            <div key={p.id} className='telaInformaticaCard'>
+
+              <div className='telaInformaticaImg' >
+                <img className='img' src={p.img}></img>
+              </div>
+
+              <div className='' >
+                {p.nome}
+              </div>
+
+              <div className=''>
+                $:{p.preco}
+              </div>
+
+              <div className='button' onClick={() => {
+                adicionarFavoritos(p);
+                quantFavSaoContar();
+
+                console.log(p)
+              }}>Adicionar </div>
+
+              <div className='button' onClick={() => {
+                adicionarDenuciado(p);
+              }}>Denuciar</div>
+
+
+            </div>
+
+          )
+        }
+      </div>
+    </div>
+  )
 }
+
+export default InformaticaComponent
