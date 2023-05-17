ac1287ffa2b04d4628622f9826192f0c61723dd7
jeallyson <118531980+jeallyson@users.noreply.github.com>
2023-05-17 11:11:04 -0300

    us

--- projeto/src/components/ComponentTecnologia/TecnologiaComponent.js
+++ projeto/src/components/ComponentTecnologia/TecnologiaComponent.js
@@ -5,6 +5,9 @@ function TecnologiaComponent ({ tecnologiaProdutos, adicionarFavoritos, adiciona
     const img = ["", "./img/fone.jpg", "./img/positivo.jpg", "./img/gabinete.jpg", "./img/impressora_3d.jpg", "./img/arduino.jpg"]
 
     const [countFavoritosTecnologia, setCountFavoritosTecnologia] = useState(0);
+
+
+    const [meusProdutos, setMeusProdutos]  = useState([]); // criou useState com array vazio para listar os produtos
   
     // lista de favoritos
     const handleAddFavorito = (p) => {
@@ -21,18 +24,42 @@ function TecnologiaComponent ({ tecnologiaProdutos, adicionarFavoritos, adiciona
     };
     
     useEffect(() => {
-        // Atualize o título do documento usando a API do navegador
-        document.title = `You clicked ${"count"} times`;
+        async function getProdutos(categoria) {
+            const response = await fetch(`https://dummyjson.com/products/category/${categoria}`); // link da api que vai listar a categoria, que vão ser os smartphones
+            const dadosApi = await response.json();
+            const result = dadosApi.products.map( 
+              product => ({ // dados da api 
+                id: product.id,
+                title: product.title,
+                preco: product.price,
+                nome: product.title,
+                valor: product.price,
+                estoque: product.stock,
+                marca: product.brand,
+                categoria: product.category,
+                thumbnail: product.thumbnail
+              })
+            );
+
+            setMeusProdutos(result);
+        }
+        getProdutos('smartphones');
+
+        
     },[]);
 
     return (
         <div className="box-produto">
             <h1>Tecnologia - Jeallyson</h1>
             <h1><small>Favoritos: ({countFavoritosTecnologia})</small> <small>Denunciados: ({countDenunciadosTecnologia})</small> </h1>
-
+        <select>
+          <option value='preco'>Preço</option>
+          <option value='estoque'>Estoque</option>
+          <option value='avaliacao'>Avaliação</option>
+        </select>
             <div className="tecnologia">
             {
-                tecnologiaProdutos.map(p =>
+                meusProdutos.map(p =>
                     <div key={p.id}className="item">
                         {p.nome} R${p.valor}
                         
@@ -40,7 +67,7 @@ function TecnologiaComponent ({ tecnologiaProdutos, adicionarFavoritos, adiciona
 
                         <button onClick={()=>{handleAddDenunciado(p);}}>Denuciar</button>
 
-                        <div> <img src={img[p.id]} alt ='tecnologia' id="img-tecnologia"></img> </div>
+                        <div> <img src={p.thumbnail} alt ='tecnologia' id="img-tecnologia"></img> </div>
             
                     </div>
                 )
