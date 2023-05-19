85e46ac6c53113244c81da802587e1cd914b8aa2
End-Star01 <129421702+jacioferton@users.noreply.github.com>
2023-05-19 11:57:31 -0300

    useState

--- projeto/src/components/ComponentSalgado/SalgadoComponent.js
+++ projeto/src/components/ComponentSalgado/SalgadoComponent.js
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Link, useNavigation } from 'react-router-dom'
 
 function SalgadoComponent ({ salgadoProdutos, categoria, onAdd, adicionarFavoritos, adicionarDenuciado}) {
 
-    useEffect(() => {
+    const [den, setDen] = useState(0)
+    const [fav, setFav] = useState(0)
 
-    });
+    const favoritar = (p) => {
+        setFav(fav+1)
+        adicionarFavoritos(p)
+    }
+    const denunciar = (p) => {
+        setDen(den+1)
+        adicionarDenuciado(p)
+    }
 
     return (
         <div className="box-produto">
             <h1 id="tituloSalgado">Salgados</h1>
+            <div id="faden">
+                <h2>Salgados Favoritados: {fav}</h2>
+                <h2>Salgados Denunciados: {den}</h2>
+            </div>
             <Link to='salgados'>Ver mais</Link>
             <div id="carrosselSalgado">
                 {
@@ -20,12 +32,12 @@ function SalgadoComponent ({ salgadoProdutos, categoria, onAdd, adicionarFavorit
                                 <img id='imgSalgado' src={[p.img]}></img>
                             </div>
                             {p.valor}<br/>
-                            <button id='btnFavSalgado' onClick={()=>{
-                                adicionarFavoritos(p);
+                            <button id='btnFavSalgado' onClick={()=>{ 
+                                favoritar(p);
                                 console.log(p)
                             }}>Favoritar</button><br/>
                             <button id='btnDenSalgado' onClick={()=>{
-                                adicionarDenuciado(p);
+                                denunciar(p);
                             }}>Denuciar</button>
                         </div>
                     )
